8b37e65c399d0f85e932b1f2ef6b6193cbeef7a8
DolphinJr69 <printdolphinjunior@gmail.com>
2022-11-22 23:22:28 +0600

    test

---
+++ first_etap/day_9/day9.py
@@ -0,0 +1,34 @@
+# with open('test.txt', 'w') as file:
+#     a = input()
+#     file.write(a)
+# print('Все успешно')
+
+
+# with open('test.txt', 'r') as file:
+#     a = file.read()
+#
+# print(a)
+
+# while True:
+#     name = input('your name')
+#     with open('file.txt', 'a') as data:
+#         data.write(name)
+#         data.write('\n')
+#     if name == 'Roman':
+#         break
+
+# with open('file.txt', 'r') as file:
+#     data = file.read()
+# data = data.split('\n')
+# print(data)
+
+#PROBLEM2
+
+
+
+
+
+
+
+
+
